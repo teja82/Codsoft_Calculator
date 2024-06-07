aa5064095a39e75fa2aa0ce204ae5b75e8d9bb3c
Challa Teja <153226505+teja82@users.noreply.github.com>
2024-06-07 09:33:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const display = document.getElementById('display');
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+function appendNumber(number) {
+    display.value += number;
+}
+
+function appendOperator(operator) {
+    if (display.value === '') return;
+    display.value += operator;
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = 'Error';
+    }
+}
